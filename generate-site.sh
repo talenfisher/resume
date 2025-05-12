b99617ae305272a36747faae03d564b80351ca5e
Talen Fisher <talen.fisher@cythral.com>
2025-05-12 15:48:01 -0500

    update script

--- generate-site.sh
+++ generate-site.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+set -eo pipefail
+
 GENERATE_STATIC_FILES_ONLY=true dotnet run
+
 git stash
+git fetch origin gh-pages
 git checkout gh-pages
+
 cp -r bin/static/* .
 git add .
 git commit -m "update static site"
 git push origin gh-pages
+
 git checkout main
